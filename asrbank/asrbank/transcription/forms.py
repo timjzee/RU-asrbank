49c135abb98f63ffb8a7d1478ef9376b761ebe74
timjzee <timjzee@gmail.com>
2026-02-24 15:32:15 +0100

    Update translation function to use gettext_lazy

--- asrbank/asrbank/transcription/forms.py
+++ asrbank/asrbank/transcription/forms.py
@@ -6,7 +6,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 
